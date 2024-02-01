7d168c4e329785484be23ae7d4a14f37c5327645
Nikita Pomelov <krt3298@gmail.com>
2024-02-01 10:10:17 +0300

    .

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+
+            for (int i = 1;  i <= 2; i++) 
+            {
+                double product = 1;
+
+                for (int p = 1; p <= 3; p++)
+                {
+                    product *= Math.Log(Math.Pow(p, 3) + i);
+                }
+
+                sum += product;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
